8b9906b65f5297de4c9c10a0bbf5fdfcf4d1c6ac
Chris Milne <milne_c1@sky.com>
2025-05-27 11:33:48 +0100

    Add files via upload

---
+++ Package-Mate/Package-Mate/Package_MateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Package_MateApp.swift
+//  Package-Mate
+//
+//  Created by Chris Milne on 27/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Package_MateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
